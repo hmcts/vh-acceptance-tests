4a4bb47f33503cc379cf478d13823051326ecd84
Andressa Dantas <Andressa.DeAzevedoDantas@HMCTS.NET>
2019-10-28 20:47:06 +0000

    Created resources folder and moved files around.

--- AcceptanceTests/AcceptanceTests.Configuration/ServiceConfiguration/AdminWebServiceConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AcceptanceTests.Configuration.ServiceConfiguration
-{
-    public class AdminWebServiceConfiguration : ServiceConfigurationBase
-    {
-        public string PexipSelfTestNodeUri { get; set; }
-        public string KinlySelfTestScoreEndpointUrl { get; set; }
-    }
-}
--- AcceptanceTests/AcceptanceTests.Configuration/ServiceConfiguration/ServiceWebServiceConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AcceptanceTests.Configuration.ServiceConfiguration
-{
-    public class ServiceWebSeviceConfiguration : ServiceConfigurationBase
-    {
-        public string PexipSelfTestNodeUri { get; set; }
-        public string KinlySelfTestScoreEndpointUrl { get; set; }
-    }
-}
