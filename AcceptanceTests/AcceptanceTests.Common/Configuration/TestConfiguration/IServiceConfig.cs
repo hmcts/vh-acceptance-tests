f6791ccfd0c7dd708b8a935d6eeda641e9dbe7f5
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2019-12-03 17:40:10 +0100

    Updated VH Services

--- AcceptanceTests/AcceptanceTests.Common/Configuration/TestConfiguration/IServiceConfig.cs
+++ AcceptanceTests/AcceptanceTests.Common/Configuration/TestConfiguration/IServiceConfig.cs
@@ -2,9 +2,19 @@
 {
     public interface IServiceConfig
     {
-        string BookingsApiUrl { get; set; }
-        string BookingsApiResourceId { get; set; }
-        string UserApiUrl { get; set; }
-        string UserApiResourceId { get; set; }
+         string AdminWebUrl { get; set; }
+         string AdminWebResourceId { get; set; }
+         string BookingsApiUrl { get; set; }
+         string BookingsApiResourceId { get; set; }
+         string KinlySelfTestScoreEndpointUrl { get; set; }
+         string PexipSelfTestNodeUri { get; set; }
+         string ServiceWebUrl { get; set; }
+         string ServiceWebResourceId { get; set; }
+         string VideoApiUrl { get; set; }
+         string VideoApiResourceId { get; set; }
+         string VideoWebUrl { get; set; }
+         string VideoWebResourceId { get; set; }
+         string UserApiUrl { get; set; }
+         string UserApiResourceId { get; set; }
     }
 }
