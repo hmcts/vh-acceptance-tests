c8e79eb34ccd6aca1b8ea5bf7bd9a3410de860c9
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2019-12-03 16:50:09 +0100

    Incremented and removed build step again

--- AcceptanceTests/AcceptanceTests.Common/Configuration/TestUserConfig/TestUserBase.cs
+++ AcceptanceTests/AcceptanceTests.Common/Configuration/TestUserConfig/TestUserBase.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using AcceptanceTests.Common.Model.User;
 
 namespace AcceptanceTests.Common.Configuration.TestUserConfig
