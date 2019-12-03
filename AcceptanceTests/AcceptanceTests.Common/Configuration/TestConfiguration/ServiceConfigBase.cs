5fe51c00db2b29b09173af29578ad1d41ecb394c
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2019-12-03 17:37:50 +0100

    Added missing resources

--- AcceptanceTests/AcceptanceTests.Common/Configuration/TestConfiguration/ServiceConfigBase.cs
+++ AcceptanceTests/AcceptanceTests.Common/Configuration/TestConfiguration/ServiceConfigBase.cs
@@ -2,8 +2,18 @@
 {
     public class ServiceConfigBase : IServiceConfig
     {
+        public string AdminWebUrl { get; set; }
+        public string AdminWebResourceId { get; set; }
         public string BookingsApiUrl { get; set; }
         public string BookingsApiResourceId { get; set; }
+        public string KinlySelfTestScoreEndpointUrl { get; set; }
+        public string PexipSelfTestNodeUri { get; set; }
+        public string ServiceWebUrl { get; set; }
+        public string ServiceWebResourceId { get; set; }
+        public string VideoApiUrl { get; set; }
+        public string VideoApiResourceId { get; set; }
+        public string VideoWebUrl { get; set; }
+        public string VideoWebResourceId { get; set; }
         public string UserApiUrl { get; set; }
         public string UserApiResourceId { get; set; }
     }
