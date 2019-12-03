5b8aa0e3e3e84616f0521c2faab7f41835db17cc
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2019-12-03 16:37:43 +0100

    Added the base class

---
+++ AcceptanceTests/AcceptanceTests.Common/Configuration/TestUserConfig/TestUserBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AcceptanceTests.Common.Model.User;
+
+namespace AcceptanceTests.Common.Configuration.TestUserConfig
+{
+    public class TestUserBase : ITestUser
+    {
+        public string TestClientId { get; set; }
+        public string TestClientSecret { get; set; }
+        public string TestUserPassword { get; set; }
+        public string TestUsernameStem { get; set; }
+        public List<UserAccount> UserAccounts { get; set; }
+    }
+}
