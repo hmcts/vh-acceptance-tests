c4a1e6b8ccec612d58804b49d98a848a34720fdc
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-01-22 11:41:42 +0100

    Moved question keys to NuGet package

---
+++ AcceptanceTests/AcceptanceTests.Common/Data/Questions/RepresentativeQuestionKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace AcceptanceTests.Common.Data.Questions
+{
+    public static class RepresentativeQuestionKeys
+    {
+        public const string OtherInformation = "OTHER_INFORMATION";
+        public const string PresentingTheCase = "PRESENTING_THE_CASE";
+    }
+}
