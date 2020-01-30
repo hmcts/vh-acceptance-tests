e6ba96730473265aac7d67999739e59ae572e2ae
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-01-30 15:10:37 +0100

    Attempt to get Edge driver working

--- AcceptanceTests/AcceptanceTests.Common/Driver/Strategies/EdgeDriverStrategy.cs
+++ AcceptanceTests/AcceptanceTests.Common/Driver/Strategies/EdgeDriverStrategy.cs
@@ -8,8 +8,10 @@ namespace AcceptanceTests.Common.Driver.Strategies
     {
         public override RemoteWebDriver InitialiseForSauceLabs()
         {
-            var edgeOptions = new EdgeOptions() { PlatformName = "Windows 10", BrowserVersion = "18.17763" , UnhandledPromptBehavior = UnhandledPromptBehavior.Accept };
-            edgeOptions.AddAdditionalCapability("dom.webnotifications.enabled", true);
+            var edgeOptions = new EdgeOptions() { PlatformName = "Windows 10", BrowserVersion = "18.17763" };
+            edgeOptions.AddAdditionalCapability(CapabilityType.UnexpectedAlertBehavior, "accept");
+            edgeOptions.AddAdditionalCapability(CapabilityType.UnhandledPromptBehavior, UnhandledPromptBehavior.Accept);
+
             if (!BlockedCamAndMic)
             {
                 edgeOptions.AddAdditionalCapability("permissions.default.microphone", true);
@@ -27,6 +29,9 @@ namespace AcceptanceTests.Common.Driver.Strategies
                 UnhandledPromptBehavior = UnhandledPromptBehavior.Accept,
                 UseInPrivateBrowsing = true
             };
+            edgeOptions.AddAdditionalCapability(CapabilityType.UnexpectedAlertBehavior, "accept");
+            edgeOptions.AddAdditionalCapability("permissions.default.microphone", true);
+            edgeOptions.AddAdditionalCapability("permissions.default.camera", true);
             return new EdgeDriver("C:\\Windows\\system32\\", edgeOptions, LocalTimeout);
         }
     }
