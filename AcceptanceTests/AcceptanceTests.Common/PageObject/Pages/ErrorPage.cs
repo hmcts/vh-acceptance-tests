0067ddcfedd8c8a8163ab661971e7b3048edabf3
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-02-02 10:14:06 +0100

    Added locator for Ie11 error page

--- AcceptanceTests/AcceptanceTests.Common/PageObject/Pages/ErrorPage.cs
+++ AcceptanceTests/AcceptanceTests.Common/PageObject/Pages/ErrorPage.cs
@@ -9,9 +9,7 @@ namespace AcceptanceTests.Common.PageObject.Pages
         private const string PasteError = "If you pasted the web address, check you copied the entire address.";
         private const string LinkError = "If the web address is correct or you selected a link or button, contact us using the options below.";
         private const string NotRegisteredError = "It looks like you are not registered for this service.";
-        private const string IsThisAMistakeError =
-            "If you think this is a mistake and you need to speak to someone, please contact us using the options below.";
-
+        private const string IsThisAMistakeError = "If you think this is a mistake and you need to speak to someone, please contact us using the options below.";
         public static By NotFoundPageTitle = CommonLocators.ElementContainingText("Page not found");
         public static By UnauthorisedPageTitle = CommonLocators.ElementContainingText("You are not authorised to use this service");
         public static By TypedErrorMessage = CommonLocators.ElementContainingText(TypedError);
@@ -20,5 +18,6 @@ namespace AcceptanceTests.Common.PageObject.Pages
         public static By NotRegisteredErrorMessage = CommonLocators.ElementContainingText(NotRegisteredError);
         public static By IsThisAMistakeErrorMessage = CommonLocators.ElementContainingText(IsThisAMistakeError);
         public static By UnsupportedBrowserTitle = By.Id("unsupported");
+        public static By UnsupportedBrowserTitleIe = By.Id("unsupportedIe11");
     }
 }
