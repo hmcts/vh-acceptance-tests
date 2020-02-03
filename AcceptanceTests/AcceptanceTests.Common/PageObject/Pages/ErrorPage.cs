720df8d321c1b53749ba9408fbc8aa286f0b5005
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-02-03 10:29:15 +0100

    Unsupported browser

--- AcceptanceTests/AcceptanceTests.Common/PageObject/Pages/ErrorPage.cs
+++ AcceptanceTests/AcceptanceTests.Common/PageObject/Pages/ErrorPage.cs
@@ -10,6 +10,7 @@ namespace AcceptanceTests.Common.PageObject.Pages
         private const string LinkError = "If the web address is correct or you selected a link or button, contact us using the options below.";
         private const string NotRegisteredError = "It looks like you are not registered for this service.";
         private const string IsThisAMistakeError = "If you think this is a mistake and you need to speak to someone, please contact us using the options below.";
+        private const string UnsupportedBrowser = "You've signed in using an unsupported browser";
         public static By NotFoundPageTitle = CommonLocators.ElementContainingText("Page not found");
         public static By UnauthorisedPageTitle = CommonLocators.ElementContainingText("You are not authorised to use this service");
         public static By TypedErrorMessage = CommonLocators.ElementContainingText(TypedError);
@@ -19,5 +20,6 @@ namespace AcceptanceTests.Common.PageObject.Pages
         public static By IsThisAMistakeErrorMessage = CommonLocators.ElementContainingText(IsThisAMistakeError);
         public static By UnsupportedBrowserTitle = By.Id("unsupported");
         public static By UnsupportedBrowserTitleIe = By.Id("unsupportedIe11");
+        public static By UnsupportedBrowserText = CommonLocators.ElementContainingText(UnsupportedBrowser);
     }
 }
