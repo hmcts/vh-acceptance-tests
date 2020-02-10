bc8f8dd87057d8e7169114db4943e1771060e7d4
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-02-10 18:56:55 +0100

    Attempt with desired capabailities

--- AcceptanceTests/AcceptanceTests.Common/Driver/Strategies/EdgeDriverStrategy.cs
+++ AcceptanceTests/AcceptanceTests.Common/Driver/Strategies/EdgeDriverStrategy.cs
@@ -19,7 +19,7 @@ namespace AcceptanceTests.Common.Driver.Strategies
             browserOptions.AddAdditionalCapability("permissions.default.camera", 1);
             browserOptions.AddAdditionalCapability("avoidProxy", true);
             browserOptions.AddAdditionalCapability("sauce:options", SauceOptions);
-            return new RemoteWebDriver(Uri, browserOptions);
+            return new RemoteWebDriver(Uri, browserOptions.ToCapabilities(), SauceLabsTimeout);
         }
 
         public override IWebDriver InitialiseForLocal()
