b87e301d9e4fe04957ad60b98c8ac9bfd7ce32c1
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-02-21 16:16:00 +0100

    Add a helper for text in safari

---
+++ AcceptanceTests/AcceptanceTests.Common/Test/Helpers/TextHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AcceptanceTests.Common.Test.Helpers
+{
+    public static class TextHelpers
+    {
+        public static string RemoveSpacesOnSafari(string text)
+        {
+            text = RemoveWhitespace(text);
+            text = RemoveTabs(text);
+            text = RemoveNewLines(text);
+            text = RemoveDoubleWhiteSpaces(text);
+            return text;
+        }
+
+        private static string RemoveWhitespace(string text)
+        {
+            return text.Trim();
+        }
+
+        private static string RemoveTabs(string text)
+        {
+            return text.Replace("\t", " ");
+        }
+
+        private static string RemoveNewLines(string text)
+        {
+            return text.Replace("\n", " ");
+        }
+
+        private static string RemoveDoubleWhiteSpaces(string text)
+        {
+            while (text.IndexOf("  ", StringComparison.Ordinal) >= 0)
+            {
+                text = text.Replace("  ", " ");
+            }
+            return text;
+        }
+    }
+}
