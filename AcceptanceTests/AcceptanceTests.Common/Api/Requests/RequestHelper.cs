808b3c3c7733071622541ba7403e9eedfe1243ab
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-03-10 18:13:47 +0100

    Added more to the api request helpers

--- AcceptanceTests/AcceptanceTests.Common/Api/Requests/RequestHelper.cs
+++ AcceptanceTests/AcceptanceTests.Common/Api/Requests/RequestHelper.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace AcceptanceTests.Common.Api.Requests
@@ -31,6 +33,22 @@ namespace AcceptanceTests.Common.Api.Requests
                 ContractResolver = contractResolver,
                 Formatting = Formatting.Indented
             });
+        }
+
+        public static HttpResponseMessage CreateHttpResponseMessage(object serializeObject, HttpStatusCode httpStatusCode)
+        {
+            return new HttpResponseMessage(httpStatusCode)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(serializeObject), System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+
+        public static HttpResponseMessage CreateHttpResponseMessage(string content, HttpStatusCode httpStatusCode)
+        {
+            return new HttpResponseMessage(httpStatusCode)
+            {
+                Content = new StringContent(content)
+            };
         }
     }
 }
