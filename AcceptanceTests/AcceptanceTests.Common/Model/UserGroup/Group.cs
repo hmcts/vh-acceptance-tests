53194219f711804f53430c8b4e53704c9128babc
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-03-10 16:54:21 +0100

    Added group for UserApi

---
+++ AcceptanceTests/AcceptanceTests.Common/Model/UserGroup/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace AcceptanceTests.Common.Model.UserGroup
+{
+    public class Group
+    {
+        public string GroupId { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
