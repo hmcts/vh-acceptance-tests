bd51c7ea4021607f3471735efc9184290812aabf
Saravanan Radha Krishnan <Saravanan.Krishnan@HMCTS.NET>
2020-03-28 16:49:36 +0000

    Added Zap functionallity

---
+++ AcceptanceTests/AcceptanceTests.Common/Configuration/ZapConfiguration.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AcceptanceTests.Common.Configuration
+{
+    public class ZapConfiguration
+    {
+        public string ApiAddress { get; set; }
+        public int ApiPort { get; set; }
+        public string ApiConfigPath { get; set; }
+        public bool ZapScan { get; set; }
+        public bool ActiveScan { get; set; }
+        public bool SetUpProxy { get; set; }
+        public string ServiceName { get; set; }
+        public string SolutionFolderName { get; set; }
+    }
+}
