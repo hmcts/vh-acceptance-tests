2faec09dc1d7869348adfac802c1bccd648bc11a
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-03-29 15:53:44 +0200

    Added more capabilities for Edge Chromium on sauce labs.

--- AcceptanceTests/AcceptanceTests.Common/Driver/Strategies/Desktop/Windows/EdgeChromiumDriverStrategy.cs
+++ AcceptanceTests/AcceptanceTests.Common/Driver/Strategies/Desktop/Windows/EdgeChromiumDriverStrategy.cs
@@ -1,27 +1,31 @@
 ﻿using System.Collections.Generic;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Remote;
 
 namespace AcceptanceTests.Common.Driver.Strategies.Desktop.Windows
 {
     internal class EdgeChromiumDriverStrategy : Drivers
     {
+        [System.Obsolete]
         public override RemoteWebDriver InitialiseForSauceLabs()
         {
-            var browserOptions = new EdgeOptions()
+            var capabilities = new DesiredCapabilities(new Dictionary<string, object>()
             {
-                PlatformName = "Windows 10",
-                BrowserVersion = "latest",
-                UseInPrivateBrowsing = true
-            };
+                { "browserName", "MicrosoftEdge" },
+                { "platformName", "Windows 10" },
+                { "browserVersion", "latest" },
+                { "ms:inPrivate", true },
+                { "ms:edgeOptions", new Dictionary<string, object>() {
+                    {  "binary", @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" },
+                    {  "args", new List<string> { "use-fake-ui-for-media-stream", "use-fake-device-for-media-stream" } }
+                }},
+                { "sauce:options", SauceOptions}
+            });
 
-            browserOptions.AddAdditionalCapability("args", new List<string> { "use-fake-ui-for-media-stream", "use-fake-device-for-media-stream" });
-            browserOptions.AddAdditionalCapability("sauce:options", SauceOptions);
-
-            return new RemoteWebDriver(Uri, browserOptions.ToCapabilities(), SauceLabsTimeout);
+            return new RemoteWebDriver(Uri, capabilities, SauceLabsTimeout);
         }
 
+        [System.Obsolete]
         public override IWebDriver InitialiseForLocal()
         {
             var capabilities = new DesiredCapabilities(new Dictionary<string, object>()
