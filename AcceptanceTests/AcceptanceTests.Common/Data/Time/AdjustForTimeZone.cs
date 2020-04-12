7e1dae629245b3a072f7b83b2c9b9998ea44d439
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-04-12 11:26:45 +0200

    Another attempt with Sauce Labs datetime

--- AcceptanceTests/AcceptanceTests.Common/Data/Time/AdjustForTimeZone.cs
+++ AcceptanceTests/AcceptanceTests.Common/Data/Time/AdjustForTimeZone.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using AcceptanceTests.Common.Driver.Support;
 
 namespace AcceptanceTests.Common.Data.Time
 {
     public class TimeZone
     {
         private readonly bool _runningOnSauceLabs;
-        public TimeZone(bool runningOnSaucelabs)
+        private readonly TargetBrowser _browser;
+        private readonly bool _isVideoWeb;
+        public TimeZone(bool runningOnSaucelabs, TargetBrowser browser, bool isVideoWeb = false)
         {
             _runningOnSauceLabs = runningOnSaucelabs;
+            _browser = browser;
+            _isVideoWeb = isVideoWeb;
         }
 
         public DateTime Adjust(DateTime dateTime)
         {
-            return !_runningOnSauceLabs ? dateTime.ToLocalTime() : dateTime.ToUniversalTime();
+            if (!_runningOnSauceLabs) return dateTime.ToLocalTime();
+            if (_browser == TargetBrowser.Safari ||
+                _browser == TargetBrowser.MacChrome ||
+                _browser == TargetBrowser.MacFirefox)
+            {
+                return _isVideoWeb ? dateTime.ToUniversalTime() : dateTime.ToUniversalTime().AddHours(1);
+            }
+            return dateTime.ToUniversalTime();
         }
     }
 }
