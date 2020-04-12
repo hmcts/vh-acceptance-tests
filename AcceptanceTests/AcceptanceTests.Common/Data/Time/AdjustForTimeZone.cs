c4909f2c20655e7647f801b094d6ed667db20f67
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-04-12 10:52:53 +0200

    Adjust timezone for Safari

--- AcceptanceTests/AcceptanceTests.Common/Data/Time/AdjustForTimeZone.cs
+++ AcceptanceTests/AcceptanceTests.Common/Data/Time/AdjustForTimeZone.cs
@@ -1,28 +1,18 @@
 ﻿using System;
-using AcceptanceTests.Common.Driver.Support;
 
 namespace AcceptanceTests.Common.Data.Time
 {
     public class TimeZone
     {
         private readonly bool _runningOnSauceLabs;
-        private readonly TargetBrowser _browser;
-        public TimeZone(bool runningOnSaucelabs, TargetBrowser browser)
+        public TimeZone(bool runningOnSaucelabs)
         {
             _runningOnSauceLabs = runningOnSaucelabs;
-            _browser = browser;
         }
 
         public DateTime Adjust(DateTime dateTime)
         {
-            if (!_runningOnSauceLabs) return dateTime.ToLocalTime();
-            if (_browser == TargetBrowser.Safari ||
-                _browser == TargetBrowser.MacChrome ||
-                _browser == TargetBrowser.MacFirefox)
-            {
-                return dateTime.ToUniversalTime().AddHours(1);
-            }
-            return dateTime.ToUniversalTime();
+            return !_runningOnSauceLabs ? dateTime.ToLocalTime() : dateTime.ToUniversalTime();
         }
     }
 }
