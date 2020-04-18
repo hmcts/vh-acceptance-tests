af18276eb604c9921189131fed596e263212cfe4
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-04-18 11:50:41 +0200

    Added a converter class

---
+++ AcceptanceTests/AcceptanceTests.Common/Test/Helpers/ConverterHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AcceptanceTests.Common.Test.Helpers
+{
+    public static class ConverterHelpers
+    {
+        private static IEnumerable<string> ConvertStringIntoArray(string options)
+        {
+            return options.Split(",");
+        }
+    }
+}
