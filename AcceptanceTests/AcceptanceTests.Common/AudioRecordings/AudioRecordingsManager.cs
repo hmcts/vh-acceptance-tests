84ee8c68c8aee600fcd4212b7d57df5530735bac
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-05-15 18:21:01 +0200

    Fixed a bug with path to audio file

--- AcceptanceTests/AcceptanceTests.Common/AudioRecordings/AudioRecordingsManager.cs
+++ AcceptanceTests/AcceptanceTests.Common/AudioRecordings/AudioRecordingsManager.cs
@@ -7,13 +7,14 @@ namespace AcceptanceTests.Common.AudioRecordings
     {
         public static string CreateNewAudioFile(string originalFileName, Guid hearingId, string path = "TestAudioFiles")
         {
-            if (!File.Exists($"{path}/{originalFileName}"))
+            var originalFile = $"{path}/{originalFileName}";
+            if (!File.Exists(originalFile))
             {
-                throw new FileNotFoundException($"Unable to find audio file with path : {path}/{originalFileName}");
+                throw new FileNotFoundException($"Unable to find audio file with path : {originalFile}");
             }
 
             var newFileName = $"{path}/{hearingId}.mp4";
-            File.Move(originalFileName, newFileName);
+            File.Move($"{originalFile}", newFileName);
             return newFileName;
         }
     }
