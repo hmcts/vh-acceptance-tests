79c5724f902bb6ece4eab83938b9e27f25513dce
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-05-20 16:52:37 +0200

    Remove exists check

--- AcceptanceTests/AcceptanceTests.Common/AudioRecordings/AudioRecordingsManager.cs
+++ AcceptanceTests/AcceptanceTests.Common/AudioRecordings/AudioRecordingsManager.cs
@@ -7,23 +7,14 @@ namespace AcceptanceTests.Common.AudioRecordings
     {
         public static string CreateNewAudioFile(string originalFileName, Guid hearingId, string path = "TestAudioFiles")
         {
-            var originalFile = $"{path}\\{originalFileName}";
-            if (!File.Exists(originalFile))
-            {
-                throw new FileNotFoundException($"Unable to find audio file with path : {originalFile}");
-            }
-
-            var newFileName = $"{path}\\{hearingId}.mp4";
+            var originalFile = $"{path}/{originalFileName}";
+            var newFileName = $"{path}/{hearingId}.mp4";
             File.Move($"{originalFile}", newFileName);
             return newFileName;
         }
 
         public static void RemoveLocalAudioFile(string filename)
         {
-            if (!File.Exists(filename))
-            {
-                throw new FileNotFoundException($"Unable to find audio file with path : {filename}");
-            }
             File.Delete(filename);
         }
     }
