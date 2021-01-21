1d956aa47be1cebb1741a74a32cc87c1dd930966
Robin Weller <robin@saucecode.co>
2021-01-21 15:40:50 +0000

    removing target environment from config builder

--- AcceptanceTests/AcceptanceTests.Common/Driver/Drivers/Desktop/Mac/SafariDriverStrategy.cs
+++ AcceptanceTests/AcceptanceTests.Common/Driver/Drivers/Desktop/Mac/SafariDriverStrategy.cs
@@ -25,7 +25,7 @@ namespace AcceptanceTests.Common.Driver.Drivers.Desktop.Mac
             var options = new SafariOptions()
             {
                 PlatformName = MacPlatform,
-                BrowserVersion = "13.1.1",
+                BrowserVersion = "14.0.2",
                 UnhandledPromptBehavior = UnhandledPromptBehavior.Accept
             };
             
