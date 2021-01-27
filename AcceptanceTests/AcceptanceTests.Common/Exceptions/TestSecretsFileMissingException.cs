048e30568cd79d816ae282ff612846d4ea82a76f
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2021-01-27 14:24:58 +0100

    feature/VIH-7058 : Added missing secrets exception

---
+++ AcceptanceTests/AcceptanceTests.Common/Exceptions/TestSecretsFileMissingException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AcceptanceTests.Common.Exceptions
+{
+    public class TestSecretsFileMissingException : Exception
+    {
+        public TestSecretsFileMissingException(string env) : base($"Missing test secrets for running against: {env}") {}
+    }
+}
