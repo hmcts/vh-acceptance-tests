5d124b884c8812e2b07856d01ffea1034ffdb87c
Robin Weller <robin@saucecode.co>
2021-03-04 14:05:51 +0000

    VIH-5552 Update beta banner locator (#33)

--- AcceptanceTests/AcceptanceTests.Common/PageObject/Pages/CommonPages.cs
+++ AcceptanceTests/AcceptanceTests.Common/PageObject/Pages/CommonPages.cs
@@ -5,13 +5,13 @@ namespace AcceptanceTests.Common.PageObject.Pages
 {
     public static class CommonPages
     {
-        public static By SignOutLink = By.PartialLinkText("Sign out");
-        public static By SignOutMessage = CommonLocators.ElementContainingText("Signed out successfully");
-        public static By SignInLink = By.PartialLinkText("here");
-        public static By QuoteYourCaseNumberText = CommonLocators.ElementContainingText("Call us on");
-        public static By ContactUsLink = CommonLocators.ElementContainingText("Contact us for help");
-        public static By ContactUsEmailText = CommonLocators.ElementContainingText("Send us a message");
-        public static By BetaBanner = CommonLocators.ElementContainingText("beta");
+        public static readonly By SignOutLink = By.PartialLinkText("Sign out");
+        public static readonly By SignOutMessage = CommonLocators.ElementContainingText("Signed out successfully");
+        public static readonly By SignInLink = By.PartialLinkText("here");
+        public static readonly By QuoteYourCaseNumberText = CommonLocators.ElementContainingText("Call us on");
+        public static readonly By ContactUsLink = CommonLocators.ElementContainingText("Contact us for help");
+        public static readonly By ContactUsEmailText = CommonLocators.ElementContainingText("Send us a message");
+        public static readonly By BetaBanner = By.XPath("//*[contains(@class,'govuk-phase-banner') and contains(text(),'beta')]");
         public static By ContactUsEmail(string email) => CommonLocators.ElementContainingText(email);
         public static By ContactUsPhone(string phone) => CommonLocators.ElementContainingText(phone);
     }
