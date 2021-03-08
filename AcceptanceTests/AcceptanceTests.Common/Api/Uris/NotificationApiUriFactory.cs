99a4ee52e88c53f31a4887a575aca5b1949648fe
Saravanan Radha Krishnan <Saravanan.Krishnan@HMCTS.NET>
2021-03-08 10:46:46 +0000

    Updated NotificationApi end points

---
+++ AcceptanceTests/AcceptanceTests.Common/Api/Uris/NotificationApiUriFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcceptanceTests.Common.Api.Uris
+{
+    public static class NotificationApiUriFactory
+    {
+        public static class NotificationEndpoints
+        {
+            private const string ApiRoot = "notification";
+            public static string GetNotificationByEmail(string email) => $"{ApiRoot}/{email}";
+
+            public static string GetNotificationByHearingAndParticipant(string notificationType, string hearingId, string participantId) => $"{ApiRoot}/{notificationType}/{hearingId}/{participantId}";
+        }
+    }
+}
