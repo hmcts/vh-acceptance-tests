964835908035487be6d2eab342df90eadafcd6d5
Michael Keane <michael.keane@hmcts.net>
2021-08-24 17:08:01 +0100

    Saucelabs fix.

--- AcceptanceTests/AcceptanceTests.Common/Configuration/SauceLabsSettingsConfig.cs
+++ AcceptanceTests/AcceptanceTests.Common/Configuration/SauceLabsSettingsConfig.cs
@@ -5,7 +5,8 @@
         public string Username { get; set; }
         public string AccessKey { get; set; }
         public string RealDeviceApiKey { get; set; }
-        public string RemoteRealDeviceServerUrl { get; set; } = "https://eu1.appium.testobject.com/wd/hub";
+        //public string RemoteRealDeviceServerUrl { get; set; } = "https://eu1.appium.testobject.com/wd/hub";
+        public string RemoteRealDeviceServerUrl { get; set; } = "https://ondemand.eu-central-1.saucelabs.com/wd/hub";
         public string RemoteServerUrl { get; set; }
 
         public bool RunningOnSauceLabs()
